0245eba8f8eda0d3df4d2e60cc63984bd2b57b1d
aaa <aaa@163.com>
2020-06-19 15:05:48 +0800

    nnnnnn

--- src/layout/components/Sidebar/index.vue
+++ src/layout/components/Sidebar/index.vue
@@ -13,6 +13,7 @@
         mode="vertical"
       >
         <sidebar-item v-for="route in routes" :key="route.path" :item="route" :base-path="route.path" />
+
       </el-menu>
     </el-scrollbar>
   </div>
@@ -26,11 +27,32 @@ import variables from '@/styles/variables.scss'
 
 export default {
   components: { SidebarItem, Logo },
+  data(){
+    return {
+      myRoutes:{}
+      
+    }
+  },
+  created(){
+    //request net  ==>myRoutes
+    //{‘/partner’:1,'/system':1}
+  }
+  ,
   computed: {
     ...mapGetters([
       'sidebar'
     ]),
     routes() {
+      //获取角色的菜单和路由比较
+      let menuList=[]
+      console.log(this.$router.options.routes)
+      // for(let s of this.$router.options.routes)
+      // {
+      //    if(myRoutes[s.path])
+      //    {
+      //      menuList.push(s)
+      //    }
+      // }
       return this.$router.options.routes
     },
     activeMenu() {
