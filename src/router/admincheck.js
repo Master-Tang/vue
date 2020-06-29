a84fcd23cf215dddf5a3d9d939b9b7807f1d11d7
aaa <aaa@163.com>
2020-06-29 22:15:33 +0800

    lk;l

--- src/router/admincheck.js
+++ src/router/admincheck.js
@@ -4,75 +4,129 @@ export default[
         path: '/admincheck',
         component: Layout,
         name: 'admincheck',
-        redirect:"index",
+        redirect:"/admincheck/assets",
         meta: { title: '伙伴管理', icon: 'example' },
         children:[
           {
-            path: 'assets/index',
+            path: 'assets',
+            redirect:"/admincheck/assets/index",
             component: () => import('@/views/crm/admincheck/assets/Index'),
-            meta: { title: '资产伙伴', icon: 'table' }
+            meta: { title: '资产伙伴', icon: 'table' },
+            children:[
+              {
+                path: 'index',
+                component: () => import('@/views/crm/admincheck/assets/List'),
+                hidden:true
+              },
+              {
+                path: 'edit',
+                component: () => import('@/views/crm/admincheck/assets/Edit'),
+                meta:{title:'编辑'},
+                hidden:true
+              },
+            ]
           },
+          
           {
-            path: 'assets/edit',
-            component: () => import('@/views/crm/admincheck/assets/Edit'),
-            meta:{title:'编辑'},
-            hidden:true
-          },
-          {
-            path: 'fund/index',
+            path: 'fund',
+            redirect:"/admincheck/fund/index",
             component: () => import('@/views/crm/admincheck/fund/Index'),
-            meta: { title: '资金伙伴', icon: 'table' }
+            meta: { title: '资金伙伴', icon: 'table' },
+            children:[
+              {
+                path: 'index',
+                component: () => import('@/views/crm/admincheck/fund/List'),
+                hidden:true
+              },
+              {
+                path: 'edit',
+                component: () => import('@/views/crm/admincheck/fund/Edit'),
+                meta:{title:'编辑'},
+                hidden:true
+              },
+            ]
           },
+          
           {
-            path: 'fund/edit',
-            component: () => import('@/views/crm/admincheck/fund/Edit'),
-            meta:{title:'编辑'},
-            hidden:true
-          },
-          {
-            path: 'exit/index',
+            path: 'exit',
+            redirect:"/admincheck/exit/index",
             component: () => import('@/views/crm/admincheck/exit/Index'),
-            meta: { title: '退出伙伴', icon: 'table' }
+            meta: { title: '退出伙伴', icon: 'table' },
+            children:[
+              {
+                path: 'index',
+                component: () => import('@/views/crm/admincheck/exit/List'),
+                hidden:true
+              },
+              {
+                path: 'edit',
+                component: () => import('@/views/crm/admincheck/exit/Edit'),
+                meta:{title:'编辑'},
+                hidden:true
+              },
+            ]
           },
+          
           {
-            path: 'exit/edit',
-            component: () => import('@/views/crm/admincheck/exit/Edit'),
-            meta:{title:'编辑'},
-            hidden:true
-          },
-          {
-            path: 'justice/index',
+            path: 'justice',
+            redirect:"/admincheck/justice/index",
             component: () => import('@/views/crm/admincheck/justice/Index'),
-            meta: { title: '司法伙伴', icon: 'table' }
+            meta: { title: '司法伙伴', icon: 'table' },
+            children:[
+              {
+                path: 'index',
+                component: () => import('@/views/crm/admincheck/justice/List'),
+                hidden:true
+              },
+              {
+                path: 'edit',
+                component: () => import('@/views/crm/admincheck/justice/Edit'),
+                meta:{title:'编辑'},
+                hidden:true
+              },
+            ]
           },
+          
           {
-            path: 'justice/edit',
-            component: () => import('@/views/crm/admincheck/justice/Edit'),
-            meta:{title:'编辑'},
-            hidden:true
-          },
-          {
-            path: 'peer/index',
+            path: 'peer',
+            redirect:"/admincheck/peer/index",
             component: () => import('@/views/crm/admincheck/peer/Index'),
-            meta: { title: '同业伙伴', icon: 'table' }
+            meta: { title: '同业伙伴', icon: 'table' },
+            children:[
+              {
+                path: 'index',
+                component: () => import('@/views/crm/admincheck/peer/List'),
+                hidden:true
+              },
+              {
+                path: 'edit',
+                component: () => import('@/views/crm/admincheck/peer/Edit'),
+                meta:{title:'编辑'},
+                hidden:true
+              },
+            ]
           },
+          
           {
-            path: 'peer/edit',
-            component: () => import('@/views/crm/admincheck/peer/Edit'),
-            meta:{title:'编辑'},
-            hidden:true
-          },
-          {
-            path: 'gov/index',
+            path: 'gov',
+            redirect:"/admincheck/gov/index",
             component: () => import('@/views/crm/admincheck/gov/Index'),
-            meta: { title: '政府伙伴', icon: 'table' }
+            meta: { title: '政府伙伴', icon: 'table' },
+            children:[
+              {
+                path: 'index',
+                component: () => import('@/views/crm/admincheck/gov/List'),
+                hidden:true
+              },
+              {
+                path: 'edit',
+                component: () => import('@/views/crm/admincheck/gov/Edit'),
+                meta:{title:'编辑'},
+                hidden:true
+              },
+            ]
           },
-          {
-            path: 'gov/edit',
-            component: () => import('@/views/crm/admincheck/gov/Edit'),
-            meta:{title:'编辑'},
-            hidden:true
-          },
+          
         ]
       }
 ]
---
+++ src/views/crm/admincheck/gov/List.vue
@@ -0,0 +1,215 @@
+<template>
+  <div class="app-container">
+    <div class="button">
+      <el-collapse v-model="activeName" accordion>
+        <el-collapse-item title="政府伙伴查询" name="1">
+          <el-form :inline="true">
+            <el-form-item label="机构名称">
+              <el-input v-model="company"></el-input>
+            </el-form-item>
+            <el-form-item label="职务">
+              <el-input v-model="post"></el-input>
+            </el-form-item>
+            <el-form-item label="覆盖地区">
+              <el-cascader
+                style="width:100%"
+                placeholder="试试搜索：无锡"
+                v-model="cities"
+                :options="provinceList"
+                :props="{value:'regionId',label:'regionName',children:'children', multiple: true }"
+                collapse-tags
+                filterable
+              ></el-cascader>
+            </el-form-item>
+            <el-form-item>
+              <el-row>
+                <el-col :span="24">
+                  <div class="grid-content bg-purple-dark">
+                    <el-button type="primary" @click="find()">查找</el-button>
+                    <el-button type="primary" @click="reset()">重置</el-button>
+                  </div>
+                </el-col>
+              </el-row>
+            </el-form-item>
+          </el-form>
+        </el-collapse-item>
+      </el-collapse>
+    </div>
+
+    <el-table
+      id="myform"
+      v-loading="listLoading"
+      :data="list"
+      element-loading-text="Loading"
+      border
+      fit
+      style="width: 100%"
+      highlight-current-row
+    >
+      <el-table-column label="伙伴姓名" align="center">
+        <template slot-scope="scope">{{ scope.row.name }}</template>
+      </el-table-column>
+      <el-table-column label="性别" align="center">
+        <template slot-scope="scope">
+          <span>{{ scope.row.sex }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="手机号" align="center">
+        <template slot-scope="scope">{{ scope.row.telephone }}</template>
+      </el-table-column>
+
+      <el-table-column label="机构名称" align="center">
+        <template slot-scope="scope">{{ scope.row.company }}</template>
+      </el-table-column>
+      <el-table-column label="岗位" align="center">
+        <template slot-scope="scope">{{ scope.row.post }}</template>
+      </el-table-column>
+
+      <el-table-column label="操作" align="center">
+        <template slot-scope="scope">
+          <el-button type="primary" size="small" @click="handleEdit(scope.row.partnerId)">编辑</el-button>
+          <el-button type="danger" size="small" @click="handleDel(scope.row.partnerId)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <el-pagination
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="currentPage"
+      :page-sizes="[5, 10, 15, 20]"
+      :page-size="pageSize"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="total"
+    ></el-pagination>
+  </div>
+</template>
+
+<script>
+import $ from "@/api/assets";
+import qs from "querystring";
+
+export default {
+  data() {
+    return {
+      activeName: "1",
+      value: "",
+      state: 0,
+      list: null,
+      val: "",
+      listLoading: true,
+      currentPage: 1,
+      pageSize: 10,
+      total: 0,
+      company: "",
+      post: "",
+      cities: [],
+      options: [],
+      provinceList: [],
+      orgTypeList: [],
+      sourceList: [],
+      relativeList: []
+    };
+  },
+  created() {
+    this.fetchData();
+    $.addInit1().then(res => {
+      if (res.success) {
+        this.sourceList = res.data.source;
+        this.provinceList = res.data.province;
+        this.orgTypeList = res.data.orgTypeList;
+        this.relativeList = res.data.relativeList;
+      }
+    });
+  },
+  methods: {
+    reset(){
+      this.company="",
+      this.post="",
+      this.cities=[]
+    },
+    find() {
+      this.listLoading = true;
+      this.list = null;
+      // console.log(this.overArea)
+      this.pushcities = [];
+      for (let i in this.cities) {
+        this.pushcities.push(this.cities[i][2]);
+      }
+      let params = qs.stringify({
+        partnerType: 6,
+        "cities[]": this.pushcities.length > 0 ? this.pushcities : null,
+        company: this.company,
+        post: this.post
+      });
+      // console.log(params);
+      $.findOrgInfo(params).then(response => {
+        // console.log(response.data);
+        this.list = response.data.list;
+        this.total = response.data.total;
+        this.listLoading = false;
+        // console.log(response.data.list);
+      });
+    },
+
+    fetchData() {
+      this.listLoading = true;
+      $.findByNameTelHide({
+        partnerType: 6,
+        pageIndex: this.currentPage,
+        pageSize: this.pageSize
+      }).then(response => {
+        // console.log(response.data);
+        this.list = response.data.list;
+        this.total = response.data.total;
+        this.listLoading = false;
+        // console.log(response.data.list);
+      });
+    },
+    handleSizeChange(val) {
+      this.pageSize = val;
+      this.currentPage = 1;
+      // console.log(this.currentPage)
+      if (this.state == 1) {
+        this.find();
+        //console.log(this.currentPage)
+      } else this.fetchData();
+    },
+    handleCurrentChange(val) {
+      this.currentPage = val;
+      //console.log(val)
+      if (this.state == 1) {
+        this.find();
+      } else this.fetchData();
+    },
+    handleEdit(id) {
+      this.$router.push({
+        path: "edit",
+        query: { id: id }
+      });
+    },
+    handleDel(id) {
+      this.$confirm("此操作将删除该伙伴, 是否继续?", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      })
+        .then(() => {
+          // console.log(id)
+          $.remove({ partnerId: id }).then(response => {
+            this.$message({
+              type: "success",
+              message: "删除成功!"
+            });
+            this.fetchData();
+          });
+        })
+        .catch(() => {
+          this.$message({
+            type: "info",
+            message: "已取消删除"
+          });
+        });
+    }
+  }
+};
+</script>
