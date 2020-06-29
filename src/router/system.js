a3941137bfc4f095dddbb4f813020d891bd3b502
aaa <aaa@163.com>
2020-06-29 22:36:19 +0800

    jkl;

--- src/router/system.js
+++ src/router/system.js
@@ -3,82 +3,115 @@ export default [
   {
     path: '/system',
     component: Layout,
-    redirect: '/system/department/index',
+    redirect: '/system/department',
     name: 'Nested',
-    meta: {
-      title: '系统设置',
-      icon: 'user'
-    },
+    meta: { title: '系统设置', icon: 'user' },
     children: [
       {
-        path: 'department/add',
-        component: () => import('@/views/crm/system/department/Add'),
-        meta: { title: '添加' },
-        hidden: true
-      },
-      {
-        path: 'department/edit',
-        component: () => import('@/views/crm/system/department/Edit'),
-        meta: { title: '编辑' },
-        hidden: true
-      },
-      {
-        path: 'department/index',
+        path: 'department',
+        redirect:"/system/department/index",
         component: () => import('@/views/crm/system/department/Index'), // Parent router-view
         meta: { title: '部门设置' },
+        children:[
+          {
+            path: 'index',
+            component: () => import('@/views/crm/system/department/List'),
+            hidden:true
+          },
+          {
+            path: 'add',
+            component: () => import('@/views/crm/system/department/Add'),
+            meta: { title: '添加' },
+            hidden: true
+          },
+          {
+            path: 'edit',
+            component: () => import('@/views/crm/system/department/Edit'),
+            meta: { title: '编辑' },
+            hidden: true
+          },
+        ]
       },
+      
+
 
       {
-        path: 'dict/index',
+        path: 'dict',
+        redirect:"/system/dict/index",
         component: () => import('@/views/crm/system/dict/Index'),
         meta: { title: '字典设置' },
-       
+        children:[
+          {
+            path: 'index',
+            component: () => import('@/views/crm/system/dict/List'),
+            hidden:true
+          },
+          {
+            path: 'add',
+            component: () => import('@/views/crm/system/dict/add'),
+            meta: { title: '添加' },
+            hidden: true
+          },
+          {
+            path: 'edit',
+            component: () => import('@/views/crm/system/dict/edit'),
+            meta: { title: '编辑' },
+            hidden: true
+          },
+        ]
       },
+      
       {
-        path: 'dict/add',
-        component: () => import('@/views/crm/system/dict/add'),
-        meta: { title: '添加' },
-        hidden: true
-      },
-      {
-        path: 'dict/edit',
-        component: () => import('@/views/crm/system/dict/edit'),
-        meta: { title: '编辑' },
-        hidden: true
-      },
-      {
-        path: 'user/index',
+        path: 'sysuser',
+        redirect:"/system/sysuser/index",
         component: () => import('@/views/crm/system/sysuser/Index'),
-        meta: { title: '用户设置' }
+        meta: { title: '用户设置' },
+        children:[
+          {
+            path: 'index',
+            component: () => import('@/views/crm/system/sysuser/List'),
+            hidden:true
+          },
+          {
+            path: 'add',
+            component: () => import('@/views/crm/system/sysuser/add'),
+            meta: { title: '添加' },
+            hidden: true
+          },
+          {
+            path: 'edit',
+            component: () => import('@/views/crm/system/sysuser/edit'),
+            meta: { title: '编辑' },
+            hidden: true
+          },
+        ]
       },
-      {
-        path: 'user/add',
-        component: () => import('@/views/crm/system/sysuser/add'),
-        meta: { title: '添加' },
-        hidden: true
-      },
-      {
-        path: 'user/edit',
-        component: () => import('@/views/crm/system/sysuser/edit'),
-        meta: { title: '编辑' },
-        hidden: true
-      },
+      
       {
         path: 'statistics/index',
         component: () => import('@/views/crm/system/statistics/index'),
         meta: { title: '绩效统计' }
       },
       {
-        path: 'aim/index',
-        component: () => import('@/views/crm/system/aim/index'),
-        meta: { title: '绩效目标' }
+        path: 'aim',
+        redirect:"/system/aim/index",
+        component: () => import('@/views/crm/system/aim/Index'),
+        meta: { title: '绩效目标' },
+        children:[
+          {
+            path: 'index',
+            component: () => import('@/views/crm/system/aim/List'),
+            hidden:true
+          },
+          {
+            path: 'edit',
+            component: () => import('@/views/crm/system/aim/edit'),
+            meta: { title: '更改目标' },
+            hidden: true
+          },
+        ]
       },
-      {
-        path: 'aim/edit',
-        component: () => import('@/views/crm/system/aim/edit'),
-        meta: { title: '更改目标' },
-        hidden: true
-      },
+      
     ]
 
   }
---
+++ src/views/crm/system/sysuser/List.vue
@@ -0,0 +1,153 @@
+<template>
+  <div class="app-container">
+    <div class="button">
+      <el-form :inline="true">
+    
+        <el-form-item>
+          <el-row>
+            <el-col :span="24">
+              <div class="grid-content bg-purple-dark">
+                <el-button type="primary" @click="$router.push('add')">添加</el-button>
+              </div>
+            </el-col>
+          </el-row>
+        </el-form-item>
+      </el-form>
+    </div>
+    <el-table
+      id="myform"
+      v-loading="listLoading"
+      :data="list"
+      element-loading-text="Loading"
+      border
+      fit
+      highlight-current-row
+    >
+      <el-table-column label="用户名">
+        <template slot-scope="scope">{{ scope.row.loginName }}</template>
+      </el-table-column>
+      <el-table-column label="部门名称" width align="center">
+        <template slot-scope="scope">
+          <span>{{ scope.row.depId }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="姓名" width align="center">
+        <template slot-scope="scope">{{ scope.row.trueName }}</template>
+      </el-table-column>
+      <el-table-column label="电话号码" width align="center">
+        <template slot-scope="scope">{{ scope.row.telephone }}</template>
+      </el-table-column>
+      <el-table-column label="钉钉号" width align="center">
+        <template slot-scope="scope">{{ scope.row.dingDing }}</template>
+      </el-table-column>
+      <el-table-column label="角色号" width align="center">
+        <template slot-scope="scope">{{ scope.row.roleId }}</template>
+      </el-table-column>
+      <el-table-column label="操作" width="150" align="center">
+        <template slot-scope="scope">
+          <el-button type="primary" size="small" @click="handleEdit(scope.row.userId)">编辑</el-button>
+          <el-button type="danger" size="small" @click="handleDel(scope.row.loginName)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <el-pagination
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="currentPage"
+      :page-sizes="[5, 10, 15, 20]"
+      :page-size="pageSize"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="total"
+    ></el-pagination>
+  </div>
+</template>
+
+<script>
+import $ from "@/api/sysuser";
+
+export default {
+  data() {
+    return {
+      value: "",
+      state: 0,
+      list: null,
+      val: "",
+      listLoading: true,
+      currentPage: 1,
+      pageSize: 10,
+      total: 0,
+      options: []
+    };
+  },
+  created() {
+    this.fetchData();
+  },
+  methods: {
+    vvv() {
+      this.currentPage = 1;
+    },
+
+    fetchData() {
+      this.listLoading = true;
+      $.getList({ pageIndex: this.currentPage, pageSize: this.pageSize }).then(
+        response => {
+          this.list = response.data.list;
+          this.total = response.data.total;
+          this.listLoading = false;
+        }
+      );
+    },
+    handleSizeChange(val) {
+      this.pageSize = val;
+      this.currentPage = 1;
+      // console.log(this.currentPage)
+      if (this.state == 1) {
+        this.find();
+        //console.log(this.currentPage)
+      } else this.fetchData();
+    },
+    handleCurrentChange(val) {
+      this.currentPage = val;
+      //console.log(val)
+      if (this.state == 1) {
+        this.find();
+      } else this.fetchData();
+    },
+
+    handleEdit(id) {
+      this.$router.push({
+        path: "edit",
+        query: { id: id }
+      });
+    },
+    handleDel(loginName) {
+      this.$confirm("此操作将删除该用户, 是否继续?", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      })
+        .then(() => {
+          //console.log(id)
+          $.remove({ loginName }).then(response => {
+            this.$message({
+              type: "success",
+              message: "删除成功!"
+            });
+            this.fetchData();
+          });
+        })
+        .catch(() => {
+          this.$message({
+            type: "info",
+            message: "已取消删除"
+          });
+        });
+    }
+  }
+};
+</script>
+<style scoped>
+.button {
+  padding-left: 1rem;
+}
+</style>
